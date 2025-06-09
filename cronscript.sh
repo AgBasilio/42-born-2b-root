ffcb705854a3fb240d13904d9d349a954fe0df00
AgBasilio <agustinbasilio@gmail.com>
2025-06-09 21:16:18 +0200

    all except monitoring and guid

---
+++ cronscript.sh
@@ -0,0 +1,12 @@
+• La arquitectura de tu sistema operativo y su versión de kernel.                       | uname -a
+• El número de núcleos físicos.                                                         | lscpu
+• El número de núcleos virtuales.                                                       | nproc
+• La memoria RAM disponible actualmente en tu servidor y su porcentaje de uso.          | free
+• La memoria disponible actualmente en tu servidor y su utilización como un porcentaje. | df
+• El porcentaje actual de uso de tus núcleos.                                           |
+• La fecha y hora del último reinicio.                                                  | who
+• Si LVM está activo o no.                                                              | 
+• El número de conexiones activas.                                                      | ss -t 
+• El número de usuarios del servidor.                                                   | w
+• La dirección IPv4 de tu servidor y su MAC (Media Access Control)                      | ip address
+• El número de comandos ejecutados con sudo.                                            | 
