5ab19bb2358421d8f4b20b5529e636a8024a209f
AgBasilio <agustinbasilio@gmail.com>
2025-06-11 12:00:21 +0200

    add a command and some explanation about bash scripting

--- cronscript.sh
+++ cronscript.sh
@@ -9,4 +9,17 @@
 • El número de conexiones activas.                                                      | ss -t 
 • El número de usuarios del servidor.                                                   | w
 • La dirección IPv4 de tu servidor y su MAC (Media Access Control)                      | ip address
-• El número de comandos ejecutados con sudo.                                            | 
+• El número de comandos ejecutados con sudo.                                            | ss
+
+Curso de BASH SCRIPTING desde CERO para PRINCIPIANTES: https://youtu.be/tNeRFm2jY7c?si=nKgqFspwMMqNaMae
+shebang (o "hashbang"), le dice al sistema con qué programa debe ejecutar el script.
+    #!/bin/bash
+    con esta linea no hace falta especificar con que ejecutar el script
+        con el shebang, para ejecutar el escript se lo invoca directamente $ ./miscript.sh
+        sin el, debemos decir con que programa se debe ejecutar: $ bash miscript.sh
+
+
+probar: Neofetch es una herramienta CLI (command-line interface) que muestra información sobre tu sistema -como la versión del kernel, el shell y el hardware- junto a un logotipo ASCII de tu distribución de Linux:
+
+Comandos para el procesamiento de textos:
+    https://persoal.citius.usc.es/tf.pena/ASR/Tema_2html/node23.html 
